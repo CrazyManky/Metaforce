6077d68106ac7513b94a09003b1245bfefa63b4d
CrazyManky <zherebchikov456@gmail.com>
2026-02-22 18:02:22 +0500

    feat: add visual detected

--- Assets/_Project/Configs/LevelConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace _Project.SOConfigs
-{
-    public class LevelConfig : MonoBehaviour
-    {
-        [field: SerializeField] public int MaxEnemies { get; private set; }
-    }
-}
---
+++ Assets/_Project/Configs/LocationConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Configs
+{
+    public class LocationConfig
+    {
+        
+    }
+}
